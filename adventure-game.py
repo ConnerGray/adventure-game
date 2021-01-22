3bad03b25c2c00af4e58430386949f8a21cccd33
Conner <Connergray15@gmail.com>
2021-01-21 19:39:30 -0500

    Added Intro

--- adventure-game.py
+++ adventure-game.py
@@ -1,1 +1,10 @@
 # Author: Conner 
+# Date: 1/21/2021
+
+# Intro
+
+name = input("What is your name?" ).lower().strip()
+ans = input("hello, " + name + " would you like to go on an adventure? (yes or no)").lower().strip()
+
+# Begining of story
+
