566da156aa33da2551200cb400b7c72ebb29b59c
Conner <Connergray15@gmail.com>
2021-01-21 20:09:01 -0500

    Added first if else statements

--- adventure-game.py
+++ adventure-game.py
@@ -7,4 +7,15 @@ name = input("What is your name?" ).lower().strip()
 ans = input("hello, " + name + " would you like to go on an adventure? (yes or no)").lower().strip()
 
 # Begining of story
+if ans == 'yes':
+    print("You wake up in the middle of the night hearing a loud bang outside of your house. ")
+    ans = input("Do you investigate the sound or go back to sleep? (sound or sleep)").lower().strip()
 
+    if ans == 'sleep':
+        print("You go back to sleep and an intruder enters your home.")
+
+    else:
+        print("You go to investigate the sound... You find that it was just your screen door.")    
+
+elif ans == "no":
+    print("Fuck you then.") 
